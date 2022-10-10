947702cec35eeeceb9fddec8c2538b191cc668e6
samanthamarberger <slatham04@gmail.com>
2022-10-10 09:28:57 -0600

    commit tree f07d5a172f75401ff90ffdd50668d488215aa4cf

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
+const { config } = require("chai");
+
 // Add your code here
+// let configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json",
+//     },
+// };
+
+function submitData (userName, userEmail) {
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail,
+        }),
+    };
+    console.log(configurationObject);
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(object) {
+        let card = document.createElement('div');
+        card.append(object.id);
+    })
+}
